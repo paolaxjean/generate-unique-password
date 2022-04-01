634f6f14c0e8a3ebcf1f0b8280e736fa3d244218
paolaxjean <pr1008631@gmail.com>
2022-04-01 01:57:30 -0700

    updated length submit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var generateBtn = document.querySelector("#generate");
-var passsword = "";
-var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
-var uppercase = document.querySelector("#uppercase");
-var numbers = document.querySelector("#numbers");
-var specialChar = document.querySelector("#special");
-var canidates = "";
+var password = "";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialChar = "!#$%&()*+,-./:;<=>?@][\^_`{|}~";
+var canidates = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./:;<=>?@][\^_`{|}~";
 var passLength = 0;
 var includeUpper = false;
 var includeLower = false;
@@ -16,29 +16,61 @@ var letter = "";
 
 // TODO: add code to generate the password here
 function generatePassword() {
-  var password = "password";
   var passLength = prompt("Enter password length.");
-  if (passLength > 150 || passLength < 5);
-  alert("Password length must be 5 to 150 characters long.");
-  
-} else if 
-  
-  return password;
+  // passwordText = passLength.
+  if (passLength > 150 || passLength < 5) {
+    alert("Password length must be 5 to 150 characters long.");
+} else if (passLength <= 150 || passLength >= 5) 
+  return password; 
 }
+
+
+
+function lowercase() {
+  var lowercase = prompt("Would you like to use lowercase letters?");
+  // function makeid(length) {
+    // var result           = '';
+    // var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    // var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ lowercase));
+   }
+   return result;
+
+}
+
 console.log(generatePassword);
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(lowercase, uppercase, numbers, specialChar, length) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return password;
+  
 
   passwordText.value = password;
 
 }
 generatePassword();
 
+// const generatePassword = (length, characters) => {
+//   let password = "";
+//   for (let i = 0; i < length; i++) {
+//     password += characters.charAt(
+//       Math.floor(Math.random() * characters.length)
+//     );
+//   }
+//   return password;
+// };
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// generateBtn.addEventListener("click", )
+// generateBtn.addEventListener("click", 
 // generateBtn.addEventListener("click", )
 

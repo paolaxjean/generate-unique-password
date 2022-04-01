0d357d10c0aed37941ad55c4eca829b959a6c1e6
paolaxjean <pr1008631@gmail.com>
2022-03-31 23:38:46 -0700

    finished asking length prompt

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # generate-unique-password
+
--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passsword = "";
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercase = document.querySelector("#uppercase");
+var numbers = document.querySelector("#numbers");
+var specialChar = document.querySelector("#special");
+var canidates = "";
+var passLength = 0;
+var includeUpper = false;
+var includeLower = false;
+var includeNumber = false;
+var includeSpecial = false;
+var letter = "";
+// Assignment Code
 
+
+// TODO: add code to generate the password here
 function generatePassword() {
   var password = "password";
-  // TODO: add code to generate the password here
-
+  var passLength = prompt("Enter password length.");
+  if (passLength > 150 || passLength < 5);
+  alert("Password length must be 5 to 150 characters long.");
+  
+} else if 
+  
   return password;
 }
+console.log(generatePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -16,6 +35,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
+generatePassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", )
+// generateBtn.addEventListener("click", )
+

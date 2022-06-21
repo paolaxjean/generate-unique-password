af656a6f89f98d1c4eed77d75f4c2e5bb6405425
paolaxjean <pr1008631@gmail.com>
2022-06-21 04:22:03 -0700

    updated scripts code

--- script.js
+++ script.js
@@ -4,73 +4,89 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialChar = "!#$%&()*+,-./:;<=>?@][\^_`{|}~";
-var canidates = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./:;<=>?@][\^_`{|}~";
-var passLength = 0;
-var includeUpper = false;
-var includeLower = false;
-var includeNumber = false;
-var includeSpecial = false;
+var canidates = "";
+var passwordlength;
 var letter = "";
+var Charcters = [];
 // Assignment Code
 
 
 // TODO: add code to generate the password here
 function generatePassword() {
-  var passLength = prompt("Enter password length.");
-  // passwordText = passLength.
-  if (passLength > 150 || passLength < 5) {
-    alert("Password length must be 5 to 150 characters long.");
-} else if (passLength <= 150 || passLength >= 5) 
-  return password; 
-}
+  var passwordlength = prompt("How many characters would you like to use?"); {
+    if (passwordlength < 8) {
+      alert('Password length must be at least 10 characters');
+      return;
+    }
+    if (passwordlength > 30 ) {
+      alert('Password length must be less than 30 characters')
+      return;
+    }
 
+    var lowercase = confirm(
+      'Click OK for lowercase charcters.'
+    );
 
+    var uppercase = confirm(
+      'Click OK for uppercase characters.'
+    );
 
-function lowercase() {
-  var lowercase = prompt("Would you like to use lowercase letters?");
-  // function makeid(length) {
-    // var result           = '';
-    // var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    // var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- lowercase));
-   }
-   return result;
+    var specialChar = confirm(
+      'Click OK for special characters.'
+    );
+    var numbers = confirm(
+      'Click OK for number characters.'
+    );
 
-}
+    
+    
+    
+    if (lowercase === true ) {
+      lowercase = lowercase.slice(lowercase);
+      selectedCharacters.push(generateRandom(lowercase));
+      
+    }
+    
+    if (uppercase === true) {
+      uppercase = uppercase.slice(uppercase);
+      selectedCharacters.push(generateRandom(uppercase)); 
+      
+    }
+    if (specialChar === true) {
+      specialChars = specialChars.slice(specialChars);
+      selectedCharacters.push(generateRandom(specialChars));
+      
+    }
+    if (numbers === true) {
+      selectedCharacters.push(generateRandom(numbers));
+    }
+    
+    if(
+      lowercase === false &&
+      uppercase === false &&
+      specialChar === false &&
+      numbers === false 
+     ) {
+        alert("You must select at least one character type");
+        return;
+      }
 
-console.log(generatePassword);
-
-// Write password to the #password input
+      var password = {
+        length: length,
+        specialChar: specialChar,
+        numbers: numbers,
+        lowercase: lowercase,
+        uppercase: uppercase,
+      };
+    
+      return password;
+    }
+    // Write password to the #password input
 function writePassword(lowercase, uppercase, numbers, specialChar, length) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  for (let i = 0; i < length; i++) {
-    password += characters.charAt(
-      Math.floor(Math.random() * characters.length)
-    );
-  }
-  return password;
   
-
   passwordText.value = password;
-
+  
+  generateBtn.addEventListener("click", writePassword);
 }
-generatePassword();
-
-// const generatePassword = (length, characters) => {
-//   let password = "";
-//   for (let i = 0; i < length; i++) {
-//     password += characters.charAt(
-//       Math.floor(Math.random() * characters.length)
-//     );
-//   }
-//   return password;
-// };
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-// generateBtn.addEventListener("click", 
-// generateBtn.addEventListener("click", )
-
